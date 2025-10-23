dbd2bb2b465d36dfc77f750657e7d8a697701631
andreimagagna <andrei@futuree.org>
2025-10-23 08:40:28 -0300

    Revert "chore(ui): remove duplicate Sonner Toaster to prevent portal removeChild race on route changes"
    This reverts commit 8b049087a009c6d41927ae6ecca21802c0b95324.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -39,7 +40,8 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-  <Toaster />
+      <Toaster />
+      <Sonner />
       <AuthProvider>
         <BrowserRouter>
         <Routes>
